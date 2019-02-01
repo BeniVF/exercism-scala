7ead949fc735d2aaeb3f3249811ef05850adcdfc
BeniVF <beni.villa@gmail.com>
2019-02-01 10:11:04 +0100

    Implement acronym exercise.

--- exercises/acronym/src/main/scala/Acronym.scala
+++ exercises/acronym/src/main/scala/Acronym.scala
@@ -1,3 +1,7 @@
 object Acronym {
-  def abbreviate(phrase: String): String = ???
+  def abbreviate(phrase: String): String = phrase
+    .split(" |-")
+    .filter(_.nonEmpty)
+    .map(_.head.toUpper)
+    .mkString("")
 }
