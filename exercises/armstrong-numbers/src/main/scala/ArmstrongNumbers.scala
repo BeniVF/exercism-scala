b17d7bb8fa0cb65128d4e1688b99da451590f05e
BeniVF <beni.villa@gmail.com>
2019-02-01 13:43:19 +0100

    Implement Armstrong numbers.

---
+++ exercises/armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -0,0 +1,8 @@
+object ArmstrongNumbers {
+
+  def isArmstrongNumber(number: Int): Boolean = {
+    number.toString.map(x => Math.pow(x.toString.toInt, number.toString.length)).sum == number
+  }
+
+
+}
