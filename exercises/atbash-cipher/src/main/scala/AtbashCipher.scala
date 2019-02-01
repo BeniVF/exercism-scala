9bceb44b33a0bfa4cf59fe65c6d99e9593a126a7
BeniVF <beni.villa@gmail.com>
2019-02-01 14:24:07 +0100

    Implement atbash cipher.

---
+++ exercises/atbash-cipher/src/main/scala/AtbashCipher.scala
@@ -0,0 +1,18 @@
+object AtbashCipher{
+
+  private val alpha = {
+    val plain = "abcdefghijklmnopqrstuvwxyz"
+    val cipher = "zyxwvutsrqponmlkjihgfedcba"
+    plain.zip(cipher).toMap
+  }
+
+  def encode(value: String): String = transpose(normalize(value)).grouped(5).mkString(" ")
+
+  def decode(value: String): String = transpose(normalize(value))
+
+  private def transpose(value: String): String =
+    value.map(x => alpha.getOrElse(x, x))
+  private def normalize(value: String): String =
+    value.toLowerCase.filter(_.isLetterOrDigit)
+
+}
