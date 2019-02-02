244b308cf4607a10c7f67188b2457da5daba8496
BeniVF <beni.villa@gmail.com>
2019-02-02 22:57:11 +0100

    Implement Sum of Multiples.

--- exercises/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ exercises/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,3 +1,10 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+
+  def sum(factors: Set[Int], limit: Int): Int =
+    (1 until limit)
+      .filter(n => factors.exists(isDivisible(n, _)))
+      .sum
+
+  private def isDivisible(number: Int, divisor: Int): Boolean = number % divisor == 0
+
 }
