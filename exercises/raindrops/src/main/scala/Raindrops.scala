75817d5ac1d68616ddc89e142abf3c7a64317d79
BeniVF <beni.villa@gmail.com>
2019-02-02 18:17:36 +0100

    Implement Raindrops.

--- exercises/raindrops/src/main/scala/Raindrops.scala
+++ exercises/raindrops/src/main/scala/Raindrops.scala
@@ -1,3 +1,18 @@
 object Raindrops {
-  def convert(n: Int): String = ???
+  private val divisorToOutput = Map(
+    3 -> "Pling",
+    5 -> "Plang",
+    7 -> "Plong"
+  )
+  def convert(number: Int): String =
+    divisorToOutput.keys
+      .foldLeft[Option[String]](None) {
+        case (acc, divisor) if isDivisible(number, divisor) =>
+          val conversion = divisorToOutput.get(divisor)
+          acc.fold(conversion)(x => conversion.map(x + _))
+        case (acc, _) => acc
+      }
+      .getOrElse(number.toString)
+
+  private def isDivisible(number: Int, divisor: Int): Boolean = number % divisor == 0
 }
