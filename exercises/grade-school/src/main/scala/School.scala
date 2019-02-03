1e29ad1ba411d1195cae3c206334b9a687e42173
BeniVF <beni.villa@gmail.com>
2019-02-03 17:23:25 +0100

    Implement Grade School.

--- exercises/grade-school/src/main/scala/School.scala
+++ exercises/grade-school/src/main/scala/School.scala
@@ -1,11 +1,18 @@
+import scala.collection.immutable._
+
 class School {
   type DB = Map[Int, Seq[String]]
 
-  def add(name: String, g: Int) = ???
+  private val dbRef = scala.collection.mutable.Map.empty[Int, List[String]]
 
-  def db: DB = ???
+  def add(name: String, g: Int): Unit = {
+    val newValue = dbRef.getOrElseUpdate(g, List.empty[String])
+    dbRef += g -> (newValue :+ name)
+  }
 
-  def grade(g: Int): Seq[String] = ???
+  def db: DB = dbRef.toMap
 
-  def sorted: DB = ???
+  def grade(g: Int): Seq[String] = dbRef.getOrElse(g, List.empty)
+
+  def sorted: DB = TreeMap(dbRef.toSeq.map { case (x, y) => x -> y.sorted }: _*)
 }
