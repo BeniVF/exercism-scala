217f91ce1252aed519d738fe4c72515f36757903
BeniVF <beni.villa@gmail.com>
2019-02-03 18:55:55 +0100

    Implement Strain.

--- exercises/strain/src/main/scala/Strain.scala
+++ exercises/strain/src/main/scala/Strain.scala
@@ -1,1 +1,20 @@
+import scala.annotation.tailrec
 
+object Strain {
+  def keep[T](seq: Seq[T], f: T => Boolean): Seq[T] = filter(seq)(f)
+
+  def discard[T](seq: Seq[T], f: T => Boolean): Seq[T] = filter(seq)(not(f))
+
+  private def not[T]: (T => Boolean) => T => Boolean = f => x => !f(x)
+
+  private def filter[T](seq: Seq[T])(f: T => Boolean): Seq[T] = {
+    @tailrec
+    def rec(rest: Seq[T], acc: Seq[T]): Seq[T] = rest match {
+      case Nil             => acc
+      case x :: xs if f(x) => rec(xs, x +: acc)
+      case _ :: xs         => rec(xs, acc)
+    }
+    rec(seq.reverse, Seq.empty[T])
+  }
+
+}
